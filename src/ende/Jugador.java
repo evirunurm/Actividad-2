aa8e5e0006ec371384eb1120f74266435325e930
Evelin <teakid21@gmail.com>
2022-05-15 19:32:30 +0200

    Convenciones de escritura: Magic Numbers

--- src/ende/Jugador.java
+++ src/ende/Jugador.java
@@ -5,25 +5,27 @@
 package ende;
 
 class Jugador {
+    public static final String PIEDRA = "piedra";
+    public static final String PAPEL = "papel";
+    public static final String TIJERAS = "tijeras";
     int éxitos;      // número de partidas ganadas
     int winTotal;
 
     /**
      * Escoge piedra, papel o tijera al azar
      */
-    public String opcionAlAzar()
-    {
+    public String opcionAlAzar() {
         String opcion="";
         Integer num = (int)(Math.random()*3);
         switch(num){
             case 0:
-                opcion=("piedra");
+                opcion= PIEDRA;
                 break;
             case 1:
-                opcion=("papel");
+                opcion= PAPEL;
                 break;
             case 2:
-                opcion=("tijeras");
+                opcion= TIJERAS;
         }
         return opcion;
     }
